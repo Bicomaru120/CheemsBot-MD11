cc0fb212b8ca76c882dc2fd62bb6c57b8fbfc5d8
Emmanuel Adebayo <bicomarudono@gmail.com>
2024-08-10 02:09:28 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,11 +19,11 @@ global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
+global.botname = 'Emmy Bot MD' //ur bot name
+global.ownernumber = '2349072642884' //ur owner number
 global.ownername = '🦄드림 가이 Xeon' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
-global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
+global.wagc = "https://whatsapp.com/channel/0029Vae26R7EFeXfbF5Rd72B"
 global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
@@ -31,7 +31,7 @@ global.packname = "Sticker By"
 global.author = "🦄드림 가이 Xeon\n\n+916909137213"
 global.creator = "916909137213@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["2349072642884"] // Premium User
 global.hituet = 0
 
 //bot sett
